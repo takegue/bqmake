ad4bf2ab3da22c68ee054d8b964073177f6a6f6c
takegue <takegue@gmail.com>
2022-09-19 01:04:33 +0900

    Impl. snapshot_table__check_and_update

--- bigquery/@default/v0/@routines/snapshot_table__check_and_update/ddl.sql
+++ bigquery/@default/v0/@routines/snapshot_table__check_and_update/ddl.sql
@@ -1,8 +1,12 @@
-create or replace procedure `v0.snaphost_table__check_and_update`(
-  destination STRUCT<project_id STRING, dataset_id STRING, table_id STRING>,
-  update_job_query STRING,
-  unique_key STRING,
-  options JSON
+create or replace procedure `v0.snapshot_table__check_and_update`(
+  in destination struct<project_id string, dataset_id string, table_id string>,
+  in sources array<struct<project_id string, dataset_id string, table_id string>>,
+  in update_job struct<
+    unique_key string
+    , query string
+    , snapshot_timestamp timestamp
+  >,
+  in options json
 )
 options(description="""Procedure to check partition stalesns and update partitions if needed.
 
@@ -15,7 +19,7 @@ Arguments
 - options: JSON value
     * dry_run: Whether to run the update job as a dry run. [Default: false].
     * tolerate_delay: The delay to tolerate before updating partitions. If newer source partitions are found but its timestamp is within this delay, the procedure will not update partitions. [Default: 30 minutes].
-    * via_temp_table: Whether to update partitions via a temporary table. [Default: false].
+    * force_expire_at: The timestamp to force expire partitions. If the destination's partition timestamp is older than this timestamp, the procedure stale the partitions. [Default: null].
     * location: BigQuery Location of job. This is used for query analysis to get dependencies. [Default: "region-us"]
 
 Examples
@@ -26,35 +30,42 @@ Examples
 """
 )
 begin
-  declare stale_partitions array<string>;
-  declare sources array<struct<project_id string, dataset_id string, table_id string>>
+  declare _stale_partitions array<string>;
+  declare _sources array<struct<project_id string, dataset_id string, table_id string>> default sources;
 
   -- Options
-  declare _options struct<dry_run BOOL, tolerate_delay INTERVAL, max_update_partition_range INTERVAL, via_temp_table BOOL> default (
-    ifnull(bool(options.dry_run), false)
-    , ifnull(bool(options.via_temp_table), false)
+  declare _options struct<dry_run BOOL, tolerate_delay INTERVAL, force_expire_at timestamp> default (
+    ifnull(safe.bool(options.dry_run), false)
+    , ifnull(safe_cast(safe.string(options.tolerate_delay) as interval), interval 0 minute)
+    , timestamp(safe.string(options.force_expire_at))
   );
 
   -- Assert invalid options
   select logical_and(if(
-    key in ('dry_run', 'via_temp_table')
+    key in ('dry_run', 'tolerate_delay', 'location', 'force_expire_at')
     , true
     , error(format("Invalid Option: name=%t in %t'", key, `options`))
   ))
   from unnest(if(`options` is not null, `bqutil.fn.json_extract_keys`(to_json_string(`options`)), [])) as key
   ;
 
-  call `v0.scan_query_referenced_tables`(sources, update_job_query, to_json(struct(true as enable_query_rewrite)));
+  if _sources is null then
+    -- Auto-detect sources
+    call `v0.scan_query_referenced_tables`(
+      _sources, update_job_query, to_json(struct(true as enable_query_rewrite))
+    );
+  end if;
+
 
   call `v0.detect_staleness`(
-    stale_partitions
+    _stale_partitions
     , destination
     , sources
-    , ['__NULL__', ['__NULL__']]
+    , ['__NULL__', ['__ANY__']]
     , to_json(struct(interval 0 hours as tolerate_delay))
   );
 
-  if ifnull(array_length(stale_partitions), 0) = 0 then
+  if ifnull(array_length(_stale_partitions), 0) = 0 then
     return;
   end if;
 
@@ -63,7 +74,7 @@ begin
     select
       format('%P', to_json(struct(
         destination
-        , sources
+        , _sources
         , _options
         , partition_range
     )))
@@ -71,7 +82,11 @@ begin
     return;
   end if;
 
-  execute immediate `v0.zgensql__snapshot_scd_type2`(destination, update_job_query, unique_key);
+  execute immediate `v0.zgensql__snapshot_scd_type2`(
+    destination, update_job_query, unique_key
+  ).updatge_dml
+    using update_job.snapshot_timestamp as timestamp
+  ;
 
   if @@row_count = 0 then
     raise using message = format('Update but No data update: %t', (update_job_query, partition_range));
---
+++ bigquery/@default/v0/@routines/snapshot_table__check_and_update/test.sql
@@ -0,0 +1,65 @@
+declare destination struct<project_id string , dataset_id string, table_id string>;
+declare unique_key string default 'station_id';
+declare snapshot_query struct<create_ddl string, validate_ddl string, profile_query string, udpate_dml string, access_tvf_ddl string>;
+declare previous_process_bytes int64;
+
+create schema if not exists `zpreview__snapshot`;
+
+set destination = (null, "zpreview__snapshot", "stations_scd_type2");
+
+execute immediate `bqmake.v0.zgensql__snapshot_scd_type2`(
+    destination
+    , "select * from `bigquery-public-data.austin_bikeshare.bikeshare_stations` limit 0"
+    , unique_key
+  ).create_ddl
+    using timestamp '2022-01-01' as timestamp
+;
+assert @@row_count is null;
+
+for t in (select as struct
+    timestamp('2022-01-01' + interval idx day) as ts
+    , *
+    from unnest([
+      struct(string(null) as msg, 0 as expected_row_count, "" as query)
+      , ("Added all records", 102, "select * from `bigquery-public-data.austin_bikeshare.bikeshare_stations`")
+
+      , ("Update all records", 204, 'select * replace("invalid status" as status) from `bigquery-public-data.austin_bikeshare.bikeshare_stations`')
+      , ("Data Reset", 204, "select * from `bigquery-public-data.austin_bikeshare.bikeshare_stations`")
+
+      , ("Change one record status", 2, 'select * replace(if(station_id in (2499), "closed", status) as status) from `bigquery-public-data.austin_bikeshare.bikeshare_stations`')
+      , ("Data Reset", 2, "select * from `bigquery-public-data.austin_bikeshare.bikeshare_stations`")
+
+      , ("Delete one record", 1, 'select * from `bigquery-public-data.austin_bikeshare.bikeshare_stations` limit 101')
+      , ("Data Reset", 1, "select * from `bigquery-public-data.austin_bikeshare.bikeshare_stations`")
+
+      , ("New one record", 1, 'select S.* replace(station_id as station_id) from `bigquery-public-data.austin_bikeshare.bikeshare_stations` as S left join unnest(if(station_id = 2499, [station_id, 249999], [station_id])) as station_id')
+      , ("Data Reset", 1, "select * from `bigquery-public-data.austin_bikeshare.bikeshare_stations`")
+
+    ]) with offset idx
+    where msg is not null
+)
+do
+  set previous_process_bytes = @@script.bytes_processed;
+
+  call `bqmake.v0.snaphost_table__check_and_update`(
+    destination
+    , null
+    , (unique_key, t.query)
+    , to_json(struct(
+        current_timestamp() as force_expire_at
+    ))
+  )
+  ;
+  select if(
+    t.expected_row_count = @@row_count
+    , format("#%d Passed: %s", t.idx, t.msg)
+    , error(format('#%d Failed: %s (%t != %t)', t.idx, t.msg, t.expected_row_count, @@row_count))
+  );
+
+  assert @@script.bytes_processed - previous_process_bytes < 32 * 1024
+    as "UPDATE process bytes must be less than latest data size + update data size"
+  ;
+end for;
+
+
+drop schema `zpreview__snapshot` cascade;
