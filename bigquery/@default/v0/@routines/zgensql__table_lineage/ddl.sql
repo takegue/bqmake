c9e8feb9adf58f1b29ff579185937124c4ae918c
takegue <takegue@gmail.com>
2022-10-22 21:27:00 +0900

    fix

--- bigquery/@default/v0/@routines/zgensql__table_lineage/ddl.sql
+++ bigquery/@default/v0/@routines/zgensql__table_lineage/ddl.sql
@@ -2,7 +2,6 @@ create or replace function `v0.zgensql__table_lineage`(
   project_id STRING
   , location STRING
   , information_schema_job_table_name STRING
-  , max_depth INT64
 )
 options(description="""Generate SQL for lineage table data
 
