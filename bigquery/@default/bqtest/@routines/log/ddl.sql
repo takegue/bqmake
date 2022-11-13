ca69c5fa7d234f89cd46dc3b526c4a4942afb1bd
takegue <takegue@gmail.com>
2022-11-14 06:41:14 +0900

    Add description

--- bigquery/@default/bqtest/@routines/log/ddl.sql
+++ bigquery/@default/bqtest/@routines/log/ddl.sql
@@ -1,6 +1,10 @@
 create or replace procedure `bqtest.log`(obj any type)
 options(
   strict_mode = false
+  , description="""BigQuery Script Logger
+  Argument `obj` is available for any type including struct, array.
+  And it should be noted that should STRUCT type will cause runtime error
+  """
 )
 begin
   execute immediate
