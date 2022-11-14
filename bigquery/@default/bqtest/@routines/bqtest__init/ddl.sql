11be2a2ee02e6c18934828eb3695a5ca926484d7
takegue <takegue@gmail.com>
2022-11-15 04:54:21 +0900

    Copy only test utilities in `bqtest__init`

--- bigquery/@default/bqtest/@routines/bqtest__init/ddl.sql
+++ bigquery/@default/bqtest/@routines/bqtest__init/ddl.sql
@@ -3,14 +3,33 @@ create or replace procedure `bqtest.bqtest__init`(
 )
 begin
   declare identifier string default format('%s.%s', coalesce(target_name.project_id, @@project_id), target_name.dataset_id);
-  execute immediate format("""
-    create or replace view `%s.zbqtest_view`
-    as
-    select * from `%s.INFORMATION_SCHEMA.VIEWS`
-    """
-    , identifier
-    , identifier
-  );
+  for routine in (
+    select
+      ddl as _origin
+      , replace(
+        `bqtest.zreplace_table_identifier`(
+          ddl
+          , (
+            "zpreview_proto.INFORMATION_SCHEMA.VIEWS"
+            , format("%s.INFORMATION_SCHEMA.VIEWS", identifier)
+          )
+        )
+        , "CREATE FUNCTION bqmake.bqtest"
+        , format("create or replace function %s", identifier)
+      ) as ddl
+    from `bqtest.INFORMATION_SCHEMA.ROUTINES`
+    where
+      routine_name in (
+        "zgensql__view_test"
+      )
+  )
+  do
+    begin
+      execute immediate routine.ddl;
+      exception when error then
+        call `bqmake.bqtest.log`(@@error.message);
+    end;
+  end for;
 end
 ;
 
@@ -19,12 +38,33 @@ begin
   set (name, init_sql, defer_sql) = `bqtest.zgensql__temporary_dataset`();
   execute immediate init_sql;
   begin
+    -- Provisioning for test
     call `bqtest.bqtest__init`((null, name));
+    execute immediate format("""
+      create or replace view `%s.%s`
+      as
+      with datasource as (
+        select * from `bigquery-public-data.austin_311.311_service_requests`
+      )
+      select * from datasource
+      """
+      , name, "derivative_view"
+    );
+
+    -- Run test case
+    execute immediate format("""
+        `bqmake.%s.zgensql__view_test`(
+        "derivative_view"
+        , [
+          ("datasource", ["unique_key"], if(false, [''], []), if(false, [('', [''])], []))
+        ]
+      )
+      """
+      , name
+    );
   exception when error then
-    select
-      -- Except Errors
-     @@error.message as message, @@error.formatted_stack_trace as formatted_stack_trace
-    ;
+    call `bqmake.bqtest.log`(struct(@@error.message as message, @@error.formatted_stack_trace as formatted_stack_trace));
   end;
+  -- Tear up for testing
   execute immediate defer_sql;
 end
