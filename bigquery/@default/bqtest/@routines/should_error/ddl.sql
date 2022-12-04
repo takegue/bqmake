7fd3f1c50ddeffb78a40f2aa9e5f978620c5d131
takegue <takegue@gmail.com>
2022-12-04 16:06:45 +0900

    Add should_error

---
+++ bigquery/@default/bqtest/@routines/should_error/ddl.sql
@@ -0,0 +1,22 @@
+create or replace procedure `bqtest.should_error`(
+  query string
+)
+begin
+  execute immediate query;
+  assert false;
+  exception when error then
+    -- Error log
+    select
+      @@error.message
+      , @@error.statement_text
+      , @@error.formatted_stack_trace
+      , @@error.stack_trace
+    ;
+    if query = @@error.statement_text then
+       raise using message = format('Query: %T must throw exception', query);
+    else
+       select "bqtest.should_errro", query, @@error.message as error_message;
+    end if;
+end;
+
+call `bqtest.should_error`("select `bqtest.sure_range`(1, 1, 10, true)");
