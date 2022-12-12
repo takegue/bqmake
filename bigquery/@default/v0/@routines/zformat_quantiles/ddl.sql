1a2802703be7ba13030bc610d19b387cc983ee6b
takegue <takegue@gmail.com>
2022-12-13 07:29:34 +0900

    Add format_quantiles function

---
+++ bigquery/@default/v0/@routines/zformat_quantiles/ddl.sql
@@ -0,0 +1,23 @@
+create  function `v0.format_quantiles`(
+  approx_array any type, picks array<int64>, round_scale int64
+)
+as (
+  array(
+    select as struct
+      100 * ix / (array_length(approx_array) - 1) as key
+      , ifnull(round(a, round_scale), a) as value
+    from unnest(approx_array) as a with offset ix
+    join unnest(if(
+      array_length(picks) > 0
+      , picks
+      , generate_array(0, array_length(approx_array)))
+    ) as ix using(ix)
+  )
+);
+
+begin
+  select
+    format_quantiles(approx_quantiles(latitude, 4), [], )
+  from `bqmake.bqtest.demo_sample_table`
+  limit 1000
+end;
