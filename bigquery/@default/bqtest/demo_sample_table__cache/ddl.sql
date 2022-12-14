1bff8fdd7a7465afc92ab623679e899473faaf66
takegue <takegue@gmail.com>
2022-12-15 06:49:59 +0900

    fix: wrong SQL

--- bigquery/@default/bqtest/demo_sample_table__cache/ddl.sql
+++ bigquery/@default/bqtest/demo_sample_table__cache/ddl.sql
@@ -1,5 +1,5 @@
 
-execute immediate 'create materialized view if exists `demo_sample_partition_table__cache` options(enable_refresh = true) as \n'
+execute immediate 'create materialized view if not exists `demo_sample_partition_table__cache` options(enable_refresh = true) as\n'
   || `bqtest.zbqt_gensql__table_profiler`(
     "demo_sample_partition_table"
     , null
