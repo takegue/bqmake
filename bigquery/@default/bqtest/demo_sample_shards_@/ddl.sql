f03304158d50557724b22e277c8fef141d6e6c4f
takegue <takegue@gmail.com>
2022-12-17 16:29:18 +0900

    chore: shorten process time for rapidly deployment

--- bigquery/@default/bqtest/demo_sample_shards_@/ddl.sql
+++ bigquery/@default/bqtest/demo_sample_shards_@/ddl.sql
@@ -1,7 +1,27 @@
+declare start date default '2019-01-01';
 
+set start = (
+  least(
+    ifnull((
+      select as value
+        date_add(max(parse_date('%Y%m%d', regexp_extract(table_name, r'\d+$'))), interval 1 day)
+      from `bqtest.INFORMATION_SCHEMA.PARTITIONS`
+      where
+        starts_with(table_name, 'demo_sample_shards_')
+        and ifnull(
+          parse_datetime('%Y%m%d', regexp_extract(table_name, r'\d+$')) > current_datetime() - interval 30 day
+          , true
+      ))
+      , start
+    )
+    , date '2023-01-01'
+  )
+);
+
+select start;
 
 FOR record in (
-  select * from unnest(generate_date_array('2019-01-01', '2022-12-31')) as d
+  select * from unnest(generate_date_array(start, '2022-12-31')) as d
 ) DO
   execute immediate format("""
     create table if not exists bqtest.demo_sample_shards_%s
