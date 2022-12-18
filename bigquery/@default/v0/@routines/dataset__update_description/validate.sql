f17dc5e9265310bdcc02a510246889903220fdcb
takegue <takegue@gmail.com>
2022-12-18 19:19:25 +0900

    Add golden test

--- bigquery/@default/v0/@routines/dataset__update_description/validate.sql
+++ bigquery/@default/v0/@routines/dataset__update_description/validate.sql
@@ -5,18 +5,24 @@ begin
   execute immediate init_sql;
   /*
    * Lineage via temporary table
-   *
    */
   create temp table `temp_table1`
   as select 1 as a from sandbox.sample_table
   ;
 
   create temp table `temp_table2`
-  as select a * 10 as A from temp_table1, (select count(1) from sandbox.sample_clone_table)
+  as
+    select a * 10 as A
+    from
+      temp_table1
+      , (select count(1) from sandbox.sample_clone_table)
   ;
 
-  create or replace table `sandbox.sample_lineage`
-  as select * from temp_table2;
+  execute immediate
+    format(
+      "create or replace table `%s.sample_lineage` as select * from temp_table2"
+      , name
+    );
 
   begin
     call `bqmake.v0.dataset__update_description`(
@@ -26,6 +32,15 @@ begin
         , current_timestamp()
         , null
       ));
+
+    call `bqmake.v0.assert_golden`(
+      (@@project_id, "zgolden", "bqmake___v0___dataset__update_description")
+      , format("select catalog_name, option_value from `%s.INFORMATION_SCHEMA.SCHEMATA_OPTIONS` where option_name = 'description' and schema_name = %T", @@project_id, name)
+      , "format('%T', (catalog_name))"
+      , @update_golden
+    );
+
+    execute immediate defer_sql;
     exception when error then
       execute immediate defer_sql;
       raise using message = format('Failed to update dataset description: %s', @@error.message);
