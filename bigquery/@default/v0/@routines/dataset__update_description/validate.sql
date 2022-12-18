3e9476cd91b592e652f8bd094008abf716479f69
takegue <takegue@gmail.com>
2022-12-18 19:32:09 +0900

    Reduction temporary table lineage (#29)
    * Remove duplicated relations from temporary dataset
    
    * Tweaking mermaid graph
    
    * Add golden test
    
    * Add `zgolden` dataset for snapshot testing

---
+++ bigquery/@default/v0/@routines/dataset__update_description/validate.sql
@@ -0,0 +1,48 @@
+begin
+  declare name, init_sql, defer_sql string;
+  set (name, init_sql, defer_sql) = (`bqmake.bqtest.zgensql__temporary_dataset`());
+
+  execute immediate init_sql;
+  /*
+   * Lineage via temporary table
+   */
+  create temp table `temp_table1`
+  as select 1 as a from `project-id-7288898082930342315.sandbox.sample_table`
+  ;
+
+  create temp table `temp_table2`
+  as
+    select a * 10 as A
+    from
+      temp_table1
+      , (select count(1) from `project-id-7288898082930342315.sandbox.sample_clone_table`)
+  ;
+
+  execute immediate
+    format(
+      "create or replace table `%s.sample_lineage` as select * from temp_table2"
+      , name
+    );
+
+  begin
+    call `bqmake.v0.dataset__update_description`(
+      [name]
+      , (
+        current_timestamp() - interval 1 hour
+        , current_timestamp()
+        , null
+      ));
+
+    call `bqmake.v0.assert_golden`(
+      (@@project_id, "zgolden", "bqmake___v0___dataset__update_description")
+      , format("select catalog_name, option_value from `%s.INFORMATION_SCHEMA.SCHEMATA_OPTIONS` where option_name = 'description' and schema_name = %T", @@project_id, name)
+      , "format('%T', (catalog_name))"
+      , @update_golden
+    );
+
+    execute immediate defer_sql;
+    exception when error then
+      execute immediate defer_sql;
+      raise using message = format('Failed to update dataset description: %s', @@error.message);
+  end;
+end
