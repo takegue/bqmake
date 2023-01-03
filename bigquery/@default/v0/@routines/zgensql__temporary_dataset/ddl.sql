ce9f89e1b8b0e6841828a39054883128f5a86326
takegue <takegue@gmail.com>
2023-01-03 12:31:27 +0900

    Deprecate zgensql__temporary_dataset (Use TABLE_STORAGE_TIMELINE instead)

--- bigquery/@default/v0/@routines/zgensql__temporary_dataset/ddl.sql
+++ bigquery/@default/v0/@routines/zgensql__temporary_dataset/ddl.sql
@@ -1,4 +1,4 @@
-create or replace function `v0.zgensql__temporary_dataset`(hidden boolean)
+create or replace function `v0.zgensql__temporary_dataset`()
 returns struct<name string, init_sql string, defer_sql string>
 as ((
   select as struct
@@ -6,13 +6,13 @@ as ((
     , format("create schema if not exists `%s` options(default_table_expiration_days=0.5)", name)
     , format("drop schema if exists `%s` cascade", name)
   from unnest([
-    if(ifnull(hidden, true), '_', '') || 'ztmp_' || replace(generate_uuid(), '-', '')
+    '_temp_' || replace(generate_uuid(), '-', '')
   ]) as name
 ));
 
 begin
   declare name, init_sql, defer_sql string;
-  set (name, init_sql, defer_sql) = (`v0.zgensql__temporary_dataset`(true));
+  set (name, init_sql, defer_sql) = (`v0.zgensql__temporary_dataset`());
   execute immediate init_sql;
   execute immediate defer_sql;
 end
