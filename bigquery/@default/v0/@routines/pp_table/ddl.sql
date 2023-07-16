aab7ba353ebd13788ed870fdacf7576d310cb217
takegue <takegue@gmail.com>
2023-07-16 22:55:40 +0900

    Add UDF: SQL-valid pretty print

---
+++ bigquery/@default/v0/@routines/pp_table/ddl.sql
@@ -0,0 +1,42 @@
+create or replace function `v0.pp_table`(table_like_object any type)
+returns string
+options(
+  description=r"""Pretty print table
+```sql
+with cte as (
+  select *
+  from unnest(
+    array<struct<
+      int int64
+      , str string
+      , float float64
+      , boolean bool
+      , ts timestamp
+      , dt datetime
+      , d date
+      , json json
+      , arr array<int64>
+      , record struct<int int64>>
+    >[
+      (1, 'hoge', 3.21234556, null, null, null, null, null, null, null),
+      (null, string(null), float64(null), bool(null), '2023-01-01', '2023-01-01', '2023-01-01', to_json(null), [1, 2], struct(1))
+    ]
+  )
+)
+
+select
+  v0.pp_table(array_agg(cte)),
+from cte
+-->
+/*-----+--------+-------+---------+----------------------+---------------------+------------+------+-------+-----------+
+| int  | str    | float | boolean | ts                   | dt                  | d          | json | arr   | record    |
++------+--------+-------+---------+----------------------+---------------------+------------+------+-------+-----------+
+|    1 | "hoge" |   3.2 |    null |                 null |                null |       null | null |  null |      null |
+| null |   null |  null |    null | 2023-01-01T00:00:00Z | 2023-01-01T00:00:00 | 2023-01-01 | null | [1,2] | {"int":1} |
++------+--------+-------+---------+----------------------+---------------------+------------+------+-------+-----------*/
+```
+"""
+)
+as (
+  prettyTable(to_json_string(val))
+);
