fa3895a79a326b49cc300754ffcd0ebe94d6ad8c
takegue <takegue@gmail.com>
2023-11-03 11:53:33 +0900

    Tweaking option and layout

--- bigquery/@default/v0/@routines/pp_sql/ddl.sql
+++ bigquery/@default/v0/@routines/pp_sql/ddl.sql
@@ -3,33 +3,48 @@ create or replace function `v0.pp_sql`(
 )
 options(
   description="""Pretty print a table-like object as a SQL format representation
+
+Arguments:
+===
+
+- table_like_object: array<any type>
+- options: JSON
+    * max_cue_rows: max number of rows to infer the type. default: 3
+    * unknown_value: filling value if type inference is not working. default: 'UNKNOWN'
   """
 )
 as ((
   select as value
     header
   from unnest([struct(
-    array(
+    'int64' as unknown_value
+    -- 'INT64' as unknown_value
+    , array( 
       select as struct
         `v0.ztypeof_columns`(format('%T', _sample), to_json_string(_sample)) as value
-      from unnest(table_like_object) as _sample
+      from unnest(table_like_object) as _sample with offset ix
       order by array_length(regexp_extract_all(format('%T', _sample), r'NULL'))
-      limit 3
+      limit 10
     ) as _signatures
   )])
   left join unnest([struct(
-    format('array<struct<%t>>[\n  %s\n]', 
-      array_to_string(
+    format("""-- AUTO-GENERATED SQL
+select * 
+from unnest(array<struct<%t>>[
+  %s
+])
+"""
+    , array_to_string(
         array(
           select
             if(
               max(s.column_name) is not null
               , format(
                   '%s %s'
-                  , ifnull(max(nullif(s.column_name, 'UNKNOWN')), 'UNKNOWN')
-                  , ifnull(max(nullif(s.type, 'UNKNOWN')), 'UNKNOWN')
+                  , ifnull(max(nullif(s.column_name, 'UNKNOWN')), unknown_value)
+                  , ifnull(max(nullif(s.type, 'UNKNOWN')), unknown_value)
                 )
-              , ifnull(max(nullif(s.type, 'UNKNOWN')), 'UNKNOWN')
+              , ifnull(max(nullif(s.type, 'UNKNOWN')), unknown_value)
             )
           from unnest(_signatures) as _signature
           left join unnest(_signature.value) as s with offset as ix
@@ -49,15 +64,17 @@ as ((
   )])
 ))
 ;
-with cte as (
-  select *
-  from unnest([
-    (null, string(null), float64(null), bool(null), '2023-01-01', '2023-01-01', '2023-01-01', to_json(null), [1, 2], struct(1)),
-    (1, 'hoge', 3.21234556, null, null, null, null, null, null, null)
-  ])
-)
+ 
+begin
+  execute immediate (
+      with cte as (
+      select *
+      from unnest([
+        (null, string(null), float64(null), bool(null), '2023-01-01', '2023-01-01', '2023-01-01', to_json(null), [1, 2], struct(1)),
+        (1, 'hoge', 3.21234556, null, null, null, null, null, null, null)
+      ])
+    )
 
-select
-  any_value(`v0.ztypeof_columns`(format('%T', cte), to_json_string(cte))) as _hoge
-  , `v0.pp_sql`(array_agg(cte)) 
-from cte
+    select as value `v0.pp_sql`(array_agg(cte)) from cte
+  );
+end;
